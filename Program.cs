8b9cffa4bd7766e85c1c61c6285a3e6576707b1a
benitopentola <javiergraciafn@gmail.com>
2022-07-04 13:38:00 +0200

    5

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace cccc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try  
+                {  
+                    int i = 555;   
+                    int r = i / 0;  
+                }  
+                    catch (DivideByZeroException ex)  
+                {  
+                    Console.WriteLine("Se ha intentado dividir entre 0. {0}", ex.Message);  
+        }
+    }
+    }
+	    }
